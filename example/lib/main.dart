b09b6f4a33c6d68f04dffa8f0fb278d41fb485ce
DucHoang-UIT <hoangpd@unicloud.com.vn>
2023-02-06 15:32:51 +0700

    optize render item in list

---
+++ example/lib/main.dart
@@ -0,0 +1,156 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:custom_loadmore/custom_loadmore.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int indexStart = 0;
+  int numberItems = 1000;
+  int itemsCount = 10000;
+  bool haveMore = true;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: false,
+        extendBody: true,
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: CustomLoadMore<int>(
+                bucketGlobal: PageStorageBucket(),
+                customScrollableLayoutBuilderInjector:
+                    MonkeyScrollableLayoutBuilderInjector(),
+                mainAxisDirection: Axis.horizontal,
+                initBuilder: (context) {
+                  return const Center(child: CircularProgressIndicator());
+                },
+                onRefresh: () {
+                  indexStart = 0;
+                  numberItems = 10;
+                  haveMore = true;
+                },
+                loadMoreBuilder: (context) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text("Hold on, loading more..."),
+                      CircularProgressIndicator(),
+                    ],
+                  );
+                },
+                initFailedBuilder: (context, retryCallback) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text("init failed"),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white38,
+                        ),
+                        onPressed: () {
+                          retryCallback.call();
+                        },
+                        child: const Text("Retry",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ],
+                  );
+                },
+                loadMoreFailedBuilder: (context, retryLoadMoreCallback) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("load more failed"),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white38,
+                        ),
+                        onPressed: () {
+                          retryLoadMoreCallback.call();
+                        },
+                        child: const Text("Retry",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ],
+                  );
+                },
+                noMoreBuilder: (context) {
+                  return const Center(child: Text("no more"));
+                },
+                listItemBuilder: (context, index, item) {
+                  return Container(
+                    color: Colors.primaries[index % Colors.primaries.length],
+                    width: 200,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text("$item"),
+                          Lottie.asset('assets/images/test2.json'),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                loadMoreCallback: (pageIndex, pageSize) async {
+                  // if (Random().nextBool()) throw Exception("load more failed");
+                  ScaffoldMessengerState scaffoldMessengerState =
+                      ScaffoldMessenger.of(context);
+                  await Future.delayed(const Duration(seconds: 1));
+                  if (numberItems > itemsCount || !haveMore) {
+                    return [];
+                  }
+                  var values =
+                      List.generate(numberItems, (index) => index + indexStart);
+                  indexStart += numberItems;
+                  SnackBar snackBar = SnackBar(
+                    content:
+                        Text('Load more success! with ${values.length} items'),
+                    duration: const Duration(microseconds: 700),
+                  );
+                  scaffoldMessengerState.showSnackBar(snackBar);
+                  if (values.last >= itemsCount) {
+                    haveMore = false;
+                  }
+                  return values;
+                },
+                shrinkWrap: false,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

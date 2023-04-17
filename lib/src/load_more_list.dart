0cbeca2d60a186945041475ee03c24cf4eb73a95
hoangpd <hoangpd@unicloud.com.vn>
2023-04-17 11:33:54 +0700

    chore

--- lib/src/load_more_list.dart
+++ lib/src/load_more_list.dart
@@ -133,55 +133,55 @@ class LoadMoreSequenceList<T> extends LoadMoreList<T> {
   }
 }
 
-class LoadMoreSectionList<T, K> extends LoadMoreList<T> {
-  final Map<K, List<T>> Function({required List<T> items}) sectionFilter;
-  final Widget Function(K key, List<Widget> children) sectionBuilder;
-  const LoadMoreSectionList(
-    super.key, {
-    required super.state,
-    required super.mainAxisDirection,
-    required super.items,
-    required super.scrollController,
-    required super.streamController,
-    required super.widget,
-    required this.sectionBuilder,
-    required this.sectionFilter,
-  });
+// class LoadMoreSectionList<T, K> extends LoadMoreList<T> {
+//   final Map<K, List<T>> Function({required List<T> items}) sectionFilter;
+//   final Widget Function(K key, List<Widget> children) sectionBuilder;
+//   const LoadMoreSectionList(
+//     super.key, {
+//     required super.state,
+//     required super.mainAxisDirection,
+//     required super.items,
+//     required super.scrollController,
+//     required super.streamController,
+//     required super.widget,
+//     required this.sectionBuilder,
+//     required this.sectionFilter,
+//   });
 
-  Map<K, List<Widget>> buildMapItems(List<T>? items, BuildContext context) {
-    if (items == null) {
-      return {};
-    }
-    final mapItems = sectionFilter(items: items);
-    final mapSection = <K, List<Widget>>{};
+//   Map<K, List<Widget>> buildMapItems(List<T>? items, BuildContext context) {
+//     if (items == null) {
+//       return {};
+//     }
+//     final mapItems = sectionFilter(items: items);
+//     final mapSection = <K, List<Widget>>{};
 
-    for (int i = 0; i < mapItems.length; i++) {
-      K key = mapItems.keys.toList()[i];
-      List<T>? items = mapItems[key];
-      final widgets = items?.map((item) {
-        return widget.listItemBuilder(context, items.indexOf(item), items);
-      }).toList();
-      mapSection[key] = widgets ?? [];
-    }
-    return mapSection;
-  }
+//     for (int i = 0; i < mapItems.length; i++) {
+//       K key = mapItems.keys.toList()[i];
+//       List<T>? items = mapItems[key];
+//       final widgets = items?.map((item) {
+//         return widget.listItemBuilder(context, items.indexOf(item), items);
+//       }).toList();
+//       mapSection[key] = widgets ?? [];
+//     }
+//     return mapSection;
+//   }
 
-  @override
-  List<Widget> buildBody(BuildContext context) {
-    List<Widget> body = [];
-    Widget? statusWidget = buildStateContent(context);
-    Map<K, List<Widget>> bodyContentRaw = buildMapItems(items, context);
-    List<Widget> bodyContent = bodyContentRaw.entries.map((e) {
-      return sectionBuilder(e.key, e.value);
-    }).toList();
+//   @override
+//   List<Widget> buildBody(BuildContext context) {
+//     List<Widget> body = [];
+//     Widget? statusWidget = buildStateContent(context);
+//     Map<K, List<Widget>> bodyContentRaw = buildMapItems(items, context);
+//     List<Widget> bodyContent = bodyContentRaw.entries.map((e) {
+//       return sectionBuilder(e.key, e.value);
+//     }).toList();
 
-    if (statusWidget == null) {
-      body = bodyContent;
+//     if (statusWidget == null) {
+//       body = bodyContent;
 
-      return body;
-    }
-    body = [...bodyContent, statusWidget];
+//       return body;
+//     }
+//     body = [...bodyContent, statusWidget];
 
-    return body;
-  }
-}
+//     return body;
+//   }
+// }
--- lib/src/scroll_layout_injector.dart
+++ lib/src/scroll_layout_injector.dart
@@ -47,32 +47,32 @@ class CustomScrollableListViewBuilderInjector<T>
   }
 }
 
-class CustomSectionListViewBuilderInjector<T, K>
-    extends CustomScrollableLayoutBuilderInjector<T> {
-  final Map<K, List<T>> Function({required List<T> items}) sectionFilter;
-  final Widget Function(K key, List<Widget> children) sectionBuilder;
+// class CustomSectionListViewBuilderInjector<T, K>
+//     extends CustomScrollableLayoutBuilderInjector<T> {
+//   final Map<K, List<T>> Function({required List<T> items}) sectionFilter;
+//   final Widget Function(K key, List<Widget> children) sectionBuilder;
 
-  CustomSectionListViewBuilderInjector(
-      {required this.sectionFilter, required this.sectionBuilder});
+//   CustomSectionListViewBuilderInjector(
+//       {required this.sectionFilter, required this.sectionBuilder});
 
-  @override
-  CustomLoadMoreContent<T> buildMainContent(
-      BuildContext context,
-      LoadMoreState state,
-      List<T>? dataItems,
-      ScrollController scrollController,
-      StreamController<LoadMoreEvent> streamController) {
-    Axis scrollDirection = widgetParent.mainAxisDirection ?? Axis.vertical;
-    return LoadMoreSectionList(
-      widgetParent.key,
-      state: state,
-      mainAxisDirection: scrollDirection,
-      items: dataItems,
-      widget: widgetParent,
-      scrollController: scrollController,
-      streamController: streamController,
-      sectionFilter: this.sectionFilter,
-      sectionBuilder: this.sectionBuilder,
-    );
-  }
-}
+//   @override
+//   CustomLoadMoreContent<T> buildMainContent(
+//       BuildContext context,
+//       LoadMoreState state,
+//       List<T>? dataItems,
+//       ScrollController scrollController,
+//       StreamController<LoadMoreEvent> streamController) {
+//     Axis scrollDirection = widgetParent.mainAxisDirection ?? Axis.vertical;
+//     return LoadMoreSectionList(
+//       widgetParent.key,
+//       state: state,
+//       mainAxisDirection: scrollDirection,
+//       items: dataItems,
+//       widget: widgetParent,
+//       scrollController: scrollController,
+//       streamController: streamController,
+//       sectionFilter: this.sectionFilter,
+//       sectionBuilder: this.sectionBuilder,
+//     );
+//   }
+// }
